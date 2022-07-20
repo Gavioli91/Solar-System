553fee5ab221b5317b2b8585c9a8d309f53f8f20
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-07-20 18:03:20 -0300

    Desafio 3

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Title extends React.Component {
-    render() {
-        const { headline } = this.props;
-        return (
-            <h2>{headline}</h2>
-        );
-    }
+  render() {
+    const { headline } = this.props;
+    return (
+      <h2>{headline}</h2>
+    );
+  }
 }
+Title.prototypes = {
+  headline: PropTypes.string.isRequired,
+};
 
 export default Title;
