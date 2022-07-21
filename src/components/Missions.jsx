a896107a58c773d641ef876efa578ccd7fb8dbba
Leandro Gavioli <leandro.gavioli@yahoo.com.br>
2022-07-21 15:12:17 -0300

    Requisito 9

---
+++ src/components/Missions.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Title from './Title';
+import missions from '../data/missions';
+import MissionCard from './MissionCard';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">
+        <Title headlines="Missões" />
+
+        <div>
+
+          {missions.map((element, index) => (
+            <MissionCard
+              key={ index }
+              name={ element.name }
+              year={ element.year }
+              country={ element.country }
+              destination={ element.destination } />
+          ))}
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Missions;
